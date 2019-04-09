20776f4cfe7c98fdf1f7be77df10e491d96de4c0
huangx <349776918@qq.com>
2019-04-09 10:46:06 +0800

    After Revert "version 1.0.4  test IDEA commit"

---
+++ src/java/service/zzServiceImpl.java
@@ -0,0 +1,22 @@
+package service;
+
+/**
+ * @author huangx007
+ * @create 2019-04-09 10:16
+ */
+public class zzServiceImpl implements zzService {
+    @Override
+    public void insert() {
+        System.out.println("z");
+    }
+
+    @Override
+    public void zz() {
+
+    }
+
+    @Override
+    public void hh() {
+
+    }
+}
